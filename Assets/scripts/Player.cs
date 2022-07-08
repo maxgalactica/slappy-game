c741289f40fb77fc71fd0bbb37f5ec9c9be0cd65
Gideon Eyer <gideon.eyer@gmail.com>
2022-07-08 13:26:33 -0500

    player 1 and player 2 hand input and movement implemented (needs cleaning up later)

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     public GameObject p2HandL;
     public GameObject p2HandR;
 
+    [SerializeField] float pullbackDistance = 1.2f;
     [SerializeField] float speed;
     [SerializeField] GameObject bullet;
 
@@ -31,6 +32,7 @@ public class Player : MonoBehaviour
         startPosR = p2HandR.transform.position;
     }
 
+    // Keep this around for reference
     public virtual void Move(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
@@ -41,12 +43,34 @@ public class Player : MonoBehaviour
 
     // On the Player Input component, the FIRE input action is the right trigger, too scared of breaking everything to rename it
 
+    public void MoveP1L(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            tVal = ctx.ReadValue<float>();
+            pivotPosL.transform.rotation = Quaternion.Euler(tVal * 180, 0, 0);
+        }
+
+        if (ctx.canceled) pivotPosL.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    public void MoveP1R(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            tVal = ctx.ReadValue<float>();
+            pivotPosR.transform.rotation = Quaternion.Euler(tVal * -180, 0, 0);
+        }
+
+        if (ctx.canceled) pivotPosL.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
     public void MoveP2L(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
             tVal = ctx.ReadValue<float>();
-            p2HandL.transform.position = new Vector3(startPosL.x + tVal, startPosL.y, startPosL.z);
+            p2HandL.transform.position = new Vector3(startPosL.x - (tVal * pullbackDistance), startPosL.y, startPosL.z);
         }
     }
 
@@ -55,20 +79,16 @@ public class Player : MonoBehaviour
         if (ctx.performed)
         {
             tVal = ctx.ReadValue<float>();
-            p2HandR.transform.position = new Vector3(startPosR.x + tVal, startPosR.y, startPosR.z);
+            p2HandR.transform.position = new Vector3(startPosR.x - (tVal * pullbackDistance), startPosR.y, startPosR.z);
         }
     }
 
+    // bound to right trigger on the gamepad by default if the input action asset is initialized through the player input component
     public void Shoot(InputAction.CallbackContext ctx)
     {
         if (ctx.started)
         {
             GameObject.Instantiate(bullet, transform.position, Quaternion.identity);
         }
-    }
-
-    private void Update()
-    {
-        //transform.position = new Vector3(startPos.x + tVal, startPos.y, startPos.z);
     }
 }
