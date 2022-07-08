26b7e523b4a43fabbf04fe1c3c013b11989e5c2e
Gideon Eyer <gideon.eyer@gmail.com>
2022-07-08 12:54:17 -0500

    player two works again

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -5,6 +5,15 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public GameObject pivotPosL;
+    public GameObject pivotPosR;
+
+    public GameObject p1HandL;
+    public GameObject p1HandR;
+
+    public GameObject p2HandL;
+    public GameObject p2HandR;
+
     [SerializeField] float speed;
     [SerializeField] GameObject bullet;
 
@@ -12,26 +21,41 @@ public class Player : MonoBehaviour
 
     float tVal;
 
-    Vector3 startPos = Vector3.zero;
+    Vector3 startPosL, startPosR = Vector3.zero;
 
     int player = 0;
 
     private void Awake()
     {
-        startPos = transform.position;
+        startPosL = p2HandL.transform.position;
+        startPosR = p2HandR.transform.position;
     }
 
     public virtual void Move(InputAction.CallbackContext ctx)
     {
-
+        if (ctx.performed)
+        {
+            //transform.RotateAround(piv)
+        }
     }
 
-    public void MoveP2(InputAction.CallbackContext ctx)
+    // On the Player Input component, the FIRE input action is the right trigger, too scared of breaking everything to rename it
+
+    public void MoveP2L(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
             tVal = ctx.ReadValue<float>();
-            transform.position = new Vector3(startPos.x + tVal, startPos.y, startPos.z);
+            p2HandL.transform.position = new Vector3(startPosL.x + tVal, startPosL.y, startPosL.z);
+        }
+    }
+
+    public void MoveP2R(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            tVal = ctx.ReadValue<float>();
+            p2HandR.transform.position = new Vector3(startPosR.x + tVal, startPosR.y, startPosR.z);
         }
     }
 
